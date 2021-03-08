0748b53412b222962ae677bf351fe72ec867fc58
eddymandran <eddy.mandran@gmail.com>
2021-03-08 10:41:43 +0100

    adding css

--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search ... "/>
+      </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location"></div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -7,9 +18,67 @@
 
 
 export default {
-
-}
+name: 'app',
+data () {
+  return {
+    api_key: '6c327dc01b02fcfa6d61e05487a92ea9'
+  }
+}}
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+#app {
+  background-image: url(./assets/cold-bg.jpg);
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
+}
+
+.search-box{
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+display: block;
+width: 100%;
+padding: 15px;
+
+color: #313131;
+font-size: 20px;
+
+appearance: none;
+border: none;
+outline: none;
+background: none;
+
+box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+background-color: rgba(255, 255, 255, 0.5);
+border-radius: 0px 16px 0px 16px;
+transition: 0.4s;
+}
+
+.search-box .search-bar:focus{
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px ;
+}
+
 </style>
