eeb6e05b26fc2f6de778c922850fdba8796ac21a
eddymandran <eddy.mandran@gmail.com>
2021-03-08 11:14:32 +0100

    css done

--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -2,12 +2,17 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search ... "/>
+        <input type="text" class="search-bar" placeholder="Search ... " />
       </div>
 
       <div class="weather-wrap">
         <div class="location-box">
-          <div class="location"></div>
+          <div class="location">Lyon, FR</div>
+          <div class="date">Monday 8 March 2021</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">9°c</div>
+          <div class="weather">Rain</div>
         </div>
       </div>
     </main>
@@ -15,26 +20,26 @@
 </template>
 
 <script>
-
-
 export default {
-name: 'app',
-data () {
-  return {
-    api_key: '6c327dc01b02fcfa6d61e05487a92ea9'
-  }
-}}
+  name: "app",
+  data() {
+    return {
+      api_key: "6c327dc01b02fcfa6d61e05487a92ea9",
+    };
+  },
+};
 </script>
 
 <style>
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 
 body {
-font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 }
 
 #app {
@@ -48,37 +53,82 @@ main {
   min-height: 100vh;
   padding: 25px;
 
-  background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
 }
 
-.search-box{
+.search-box {
   width: 100%;
   margin-bottom: 30px;
 }
 
 .search-box .search-bar {
-display: block;
-width: 100%;
-padding: 15px;
+  display: block;
+  width: 100%;
+  padding: 15px;
 
-color: #313131;
-font-size: 20px;
+  color: #313131;
+  font-size: 20px;
 
-appearance: none;
-border: none;
-outline: none;
-background: none;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
 
-box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
-background-color: rgba(255, 255, 255, 0.5);
-border-radius: 0px 16px 0px 16px;
-transition: 0.4s;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
 }
 
-.search-box .search-bar:focus{
+.search-box .search-bar:focus {
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.75);
-  border-radius: 16px 0px 16px 0px ;
+  border-radius: 16px 0px 16px 0px;
 }
 
+.location-box .location {
+  color: #ffff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: rgba(0, 0, 0, 0.25);
+}
+.location-box .date {
+  color: #ffff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #ffff;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #ffff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
 </style>
